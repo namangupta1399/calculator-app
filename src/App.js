49773bf0f9d6bda7ad5420c4d37c666b73582d32
Naman Gupta <namangupta1399@gmail.com>
2021-02-17 23:44:01 +0530

    Route updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.scss";
 import { Button, Container, Grid } from "@material-ui/core";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends React.Component {
   state = {
     numOne: 0,
     numTwo: 0,
     operator: "",
-    screenVal: ''
+    screenVal: "",
   };
 
   initialState = { ...this.state };
@@ -17,8 +18,13 @@ class App extends React.Component {
       const sol = eval(
         `${this.state.numOne} ${this.state.operator} ${this.state.numTwo}`
       );
-      console.log(sol);
-      this.setState({ numOne: sol, numTwo: 0, operator: "", screenVal: sol + " " });
+      // console.log(sol);
+      this.setState({
+        numOne: sol,
+        numTwo: 0,
+        operator: "",
+        screenVal: sol + " ",
+      });
       return;
     }
     switch (item) {
@@ -26,12 +32,12 @@ class App extends React.Component {
       case "*":
       case "+":
       case "-":
-        this.setState(prevState => {
+        this.setState((prevState) => {
           return {
             ...prevState,
             operator: item,
-            screenVal: `${prevState.screenVal}${item} `
-          }
+            screenVal: `${prevState.screenVal}${item} `,
+          };
         });
         return;
       case "0":
@@ -45,20 +51,20 @@ class App extends React.Component {
       case "8":
       case "9":
         if (this.state.operator === "") {
-          this.setState(prevState => {
+          this.setState((prevState) => {
             return {
               ...prevState,
               numOne: parseInt(prevState.numOne) * 10 + parseInt(item),
-              screenVal: `${prevState.screenVal}${item} `
-            }
+              screenVal: `${prevState.screenVal}${item} `,
+            };
           });
         } else {
-          this.setState(prevState => {
+          this.setState((prevState) => {
             return {
               ...prevState,
               numTwo: parseInt(prevState.numTwo) * 10 + parseInt(item),
-              screenVal: `${prevState.screenVal}${item} `
-            }
+              screenVal: `${prevState.screenVal}${item} `,
+            };
           });
         }
         return;
@@ -68,149 +74,153 @@ class App extends React.Component {
   };
 
   render() {
-    console.log(this.state);
+    // console.log(this.state);
     return (
       <Container maxWidth="sm">
-        <div className="user-input">
-          <input
-            className="user-input__field"
-            type="text"
-            value={this.state.screenVal}
-            readOnly
-          />
-        </div>
-        <div className="button-grp">
-          <Grid container spacing={3}>
-            <Grid item xs={9}>
-              <Button
-                className="button-grp__item"
-                onClick={() => this.setState({ ...this.initialState })}
-              >
-                Clear
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "/")}
-              >
-                /
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "7")}
-              >
-                7
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "8")}
-              >
-                8
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "9")}
-              >
-                9
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "*")}
-              >
-                X
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "4")}
-              >
-                4
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "5")}
-              >
-                5
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "6")}
-              >
-                6
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "-")}
-              >
-                -
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "1")}
-              >
-                1
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "2")}
-              >
-                2
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "3")}
-              >
-                3
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "+")}
-              >
-                +
-              </Button>
-            </Grid>
-            <Grid item xs={9}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "0")}
-              >
-                0
-              </Button>
-            </Grid>
-            <Grid item xs={3}>
-              <Button
-                className="button-grp__item"
-                onClick={this.handleButtonClick.bind(this, "=")}
-              >
-                =
-              </Button>
-            </Grid>
-          </Grid>
-        </div>
+        <BrowserRouter>
+          <Route path="/">
+            <div className="user-input">
+              <input
+                className="user-input__field"
+                type="text"
+                value={this.state.screenVal}
+                readOnly
+              />
+            </div>
+            <div className="button-grp">
+              <Grid container spacing={3}>
+                <Grid item xs={9}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={() => this.setState({ ...this.initialState })}
+                  >
+                    Clear
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "/")}
+                  >
+                    /
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "7")}
+                  >
+                    7
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "8")}
+                  >
+                    8
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "9")}
+                  >
+                    9
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "*")}
+                  >
+                    X
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "4")}
+                  >
+                    4
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "5")}
+                  >
+                    5
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "6")}
+                  >
+                    6
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "-")}
+                  >
+                    -
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "1")}
+                  >
+                    1
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "2")}
+                  >
+                    2
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "3")}
+                  >
+                    3
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "+")}
+                  >
+                    +
+                  </Button>
+                </Grid>
+                <Grid item xs={9}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "0")}
+                  >
+                    0
+                  </Button>
+                </Grid>
+                <Grid item xs={3}>
+                  <Button
+                    className="button-grp__item"
+                    onClick={this.handleButtonClick.bind(this, "=")}
+                  >
+                    =
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Route>
+        </BrowserRouter>
       </Container>
     );
   }
